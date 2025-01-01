ef7d34ccf3ab10f1433d6192b686f9317ec80d11
Olfa jlali <olfa.jlali@esprit.tn>
2025-01-02 00:09:24 +0100

    fix: add retenu

--- models/document.js
+++ models/document.js
@@ -4,8 +4,9 @@ const documentSchema = new mongoose.Schema({
     buyerId: { type: String, required: true },
     number: { type: Number, required: true }, 
     date: {type: Date, required: true},
-    TTC:{type:Number,required: true},
-    ouvert:{type:Number,required:true}
+    ttc:{type:Number,required: true},
+    ouvert:{type:Number,required:true},
+    retenu:{type:Number,required:true},
     });
   const Document = mongoose.model('Document', documentSchema, 'document'); // Explicitly set collection name
   module.exports = Document;
